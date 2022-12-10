29eb097bfe0d9ee0956e2a8ba9a84324dd182790
sazalislam04 <mdsazalislam04@gmail.com>
2022-12-10 13:49:03 +0600

    connect mongodb and post all users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -9,6 +10,45 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("signup andela server running");
+});
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.bts619l.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    client.connect();
+    console.log("databse connect");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+dbConnect();
+
+const usersCollection = client.db("andela").collection("users");
+
+app.put("/users/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const user = req.body;
+    const filter = { email: email };
+    const options = { upsert: true };
+    const updatedDoc = {
+      $set: {
+        user,
+      },
+    };
+    const result = await usersCollection.updateOne(filter, updatedDoc, options);
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.listen(port, () => {
